6fe46b6536ef7ad2ef0902b8289282cb19f35e3d
christiandarvs <christiandarvin4@gmail.com>
2026-04-22 05:16:55 +0800

    chapter 8: finish quiz

--- chap_8/random_number_generator.cpp
+++ chap_8/random_number_generator.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
+namespace {
+
+unsigned int g_state{0};
 int incrementByOne() {
   static int s_state{3};
   ++s_state;
   return s_state;
 }
 
+unsigned int LCG16() {
+  g_state = 8253729 * g_state + 2396403;
+  return g_state % 32768;
+}
+
+void seedPRNG(unsigned int seed) { g_state = seed; }
+}  // namespace
 int main() {
-  std::cout << incrementByOne() << '\n';
-  std::cout << incrementByOne() << '\n';
-  std::cout << incrementByOne() << '\n';
+  // std::cout << incrementByOne() << '\n';
+  // std::cout << incrementByOne() << '\n';
+  // std::cout << incrementByOne() << '\n';
+  seedPRNG(5);
+  for (int i{1}; i <= 10; ++i) {
+    std::cout << LCG16() << '\t';
+    if (i % 5 == 0) {
+      std::cout << '\n';
+    }
+  }
   return 0;
 }
 
